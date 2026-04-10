66a3bbab7e1b3ddd3ef558127711153b97fc4246
178088 <1052355096@qq.com>
2026-04-10 23:16:09 +0800

    Update playwright.config.js

--- frontend-tests/playwright.config.js
+++ frontend-tests/playwright.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     testDir: './e2e',
     use: {
         baseURL: 'http://localhost:8081',
-        headless: false,   // 设为 false 可以看到浏览器操作过程
+        headless: true,   // 设为 false 可以看到浏览器操作过程
         viewport: { width: 1280, height: 720 },
     },
     timeout: 30000,
